3327bd500938caa3aae8a5fc65aabf87217307d3
Alex Juszczakiewicz <curiositylex@protonmail.com>
2018-12-06 00:42:22 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,2 +1,10 @@
-def turn_counts
+def turn_count(board)
+  board.count("X") + board.count("O")
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else return "O"
+  end
 end
